ef32d386c2325bf7dc5528d22cae26cee34f0ff6
vahid ara <vahidaara@gmail.com>
2022-10-23 08:18:09 +0330

    add coinSpawner class

---
+++ CoinSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    #region public variable
+    #endregion
+    #region private variable
+    #endregion
+    #region public method 
+    #endregion
+    #region private variable
+    #endregion
+
+}
